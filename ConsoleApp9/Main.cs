8b1b63266bf72e18532dd64ae5ec2756b54a935b
komiyamma <komiyama0629@gmail.com>
2024-06-02 00:06:38 +0900

    こみっと

--- ConsoleApp9/Main.cs
+++ ConsoleApp9/Main.cs
@@ -8,7 +8,8 @@ internal partial class HmGoogleGemini
     static async Task Main()
     {
         WindowsShutDownNotifier();
+        _ = Task.Run(() => StartPipe());
         var content = await GenerateContent();
-        Console.WriteLine($"Generated content: {content}");
+        // Console.WriteLine($"Generated content: {content}");
     }
 }
