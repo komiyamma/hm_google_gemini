41926189d343e9e3b2912273cd3f074777b88527
komiyamma <komiyama0629@gmail.com>
2024-07-16 23:54:47 +0900

    google gemini のライブラリのアップデート

--- HmGoogleGemini/HmGoogleGemini/obj/Release/net8.0-windows/win-x64/HmGoogleGemini.AssemblyInfo.cs
+++ HmGoogleGemini/HmGoogleGemini/obj/Release/net8.0-windows/win-x64/HmGoogleGemini.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2024 @ Akitsugu Komiyama")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.1.7")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1.7+7d091602238fdc22b4220d30e59362c6d5c7f4d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1.7+e6bc278d15ea34f85f19298bc45504afbad6709b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmGoogleGemini")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmGoogleGemini")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.1.7")]
