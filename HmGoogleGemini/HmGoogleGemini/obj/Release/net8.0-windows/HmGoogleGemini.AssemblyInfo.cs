ede492aee5ae56c0805af8261946f47d70570af0
komiyamma <komiyama0629@gmail.com>
2024-08-29 05:58:31 +0900

    バージョンバッジ更新

--- HmGoogleGemini/HmGoogleGemini/obj/Release/net8.0-windows/HmGoogleGemini.AssemblyInfo.cs
+++ HmGoogleGemini/HmGoogleGemini/obj/Release/net8.0-windows/HmGoogleGemini.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2024 @ Akitsugu Komiyama")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.1.7")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1.7+804c0bfd0380a0dfc68963686f880386b2c767eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1.7+50e171d5c577081b396e9235b2cb45bbd026cddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmGoogleGemini")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmGoogleGemini")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.1.7")]
