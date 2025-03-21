0709db1b9a30089aa8422b4235d9ffaee5930917
komiyamma <komiyama0629@gmail.com>
2025-03-21 16:13:48 +0900

    秀丸エディタ v9.35以上とした。googleのV1ライブラリを更新。

--- HmGoogleGemini/HmGoogleGemini/obj/Release/net8.0-windows/win-x64/HmGoogleGemini.AssemblyInfo.cs
+++ HmGoogleGemini/HmGoogleGemini/obj/Release/net8.0-windows/win-x64/HmGoogleGemini.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2024-2025 @ Akitsugu Komiyama")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.0.4")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0.4+e9f6f21646ec3793c92b5e22763fdc55694ec4a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0.4+77a406079c79350b34cef89e2184b663ee698bfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmGoogleGemini")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmGoogleGemini")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.2.0.4")]
